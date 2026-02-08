8456df8fcd6a98f67c2699c96e9269d5545a42d6
asif12018 <asif.request@gmail.com>
2026-02-08 19:06:22 +0600

    server setup complete

---
+++ app.ts
@@ -0,0 +1,32 @@
+
+import  express, { Application, Request, Response }   from 'express';
+import { notFound } from './src/middleware/notFound';
+
+
+
+
+
+
+
+
+
+const app:Application = express();
+
+
+
+app.get("/", (req:Request, res:Response)=>{
+    res.status(200).json({
+        success: true,
+        message:"hello world"
+    })
+});
+
+
+app.use(notFound);
+
+
+
+
+
+
+export default app
